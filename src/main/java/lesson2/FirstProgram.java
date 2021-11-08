28528cd5acf631afe34e2a54b462e3ac4390f674
one <yalialily@mail.ru>
2021-11-09 01:23:48 +0400

    lesson2: Простые арифметческие операции и вызов метода

---
+++ src/main/java/lesson2/FirstProgram.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, мир123!");
+
+        long summa = summa(39, 52);
+        System.out.println(summa);
+    }
+
+    private static long summa(int i, int i1) {
+        int result = i+ i1;
+        return result;
+    }
+}
